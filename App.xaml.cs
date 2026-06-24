7250ea0500b3b8026ae63f0ae10d03dbf10fe44e
Paul Sohns <paul.sohns@web.de>
2026-06-24 23:54:51 +0200

    Add basic window and github workflow

---
+++ App.xaml.cs
@@ -0,0 +1,7 @@
+using System.Windows;
+
+namespace ExanimapHelper;
+
+public partial class App : Application
+{
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+
+namespace ExanimapHelper;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
